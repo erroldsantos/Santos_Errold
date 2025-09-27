578022b785c269218ffb5db8c0b81d5da8077ee0
Siezz <erroldsantos25@gmail.com>
2025-09-27 13:52:58 +0800

    render1

---
+++ LavaLust/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a79d9d62c4abb3c6f450a9b29067898f0fb79fbc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a79d9d62c4abb3c6f450a9b29067898f0fb79fbc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
